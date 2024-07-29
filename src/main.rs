90c8faed7bdca7ea8905513df2cb928164c7eb4f
Chofor <betterfuture032@gmail.com>
2024-07-29 13:29:24 -0600

    conditional 1

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,11 @@
 fn main() {
-    let mut height = 190;
-    height = height - 20;
-    let result = if height > 180 {
-        "Tall"
-    } else if height > 170 {
-        "Average"
-    } else {
-        "Short"
-    };
-
-    println!("The person is {}", result);
-
-    let health = if height < 180 {"good"} else {"unknown"};
-    println!("The person's health is {}", health);
-
-    // shadowing a different type
-    let health = if height < 180 {true} else {false};
-    println!("Is the person healthy? {}", health);
+   let mut x = 1;
+   // continue looping until x > 5
+   loop { // Similar to while true
+      if x > 5 {
+         break;
+      }
+      println!("x = {}", x);
+      x += 1;
+   }
 }
