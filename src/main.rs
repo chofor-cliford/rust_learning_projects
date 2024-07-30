d4a05d8cfe14e777d9cdafc313545d8b3f035099
Chofor <betterfuture032@gmail.com>
2024-07-29 21:45:35 -0600

    chore: Refactor main.rs for improved readability and error handling

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
+use std::io;
+
 fn main() {
-for i in 1..=10 {
-    if i %2 == 0 {
-        // Skip even numbers
-        continue;
+// let name = "Python";
+
+// use match to check the value of name
+// match name {
+//     "Rust" => println!("I'm learning Rust"),
+//     "Python" => println!("I'm learning Python"),
+//     _ => println!("I'm learning something else"),
+// }
+
+    println!("Please enter a greeting:");
+
+    let mut name = String::new();
+    io::stdin().read_line(&mut name).expect("Failed to read line");
+
+    // use of match to check the value of name
+    match name.trim() {
+        "Hello" => println!("Hello!"),
+        "Hi" => println!("Hi, nice to meet you!"),
+        "Hey" => println!("Hey!"),
+        "Good Bye" => println!("Sorry to see you go!"),
+        _ => println!("I don't know what you said"),
     }
-    println!("{}", i);
-    if i == 7 {
-        // Exit the loop early
-        break;
-    }
-}
+
 }
