7a1cf4e2162e12f43b162ace626324d67d32f90a
Chofor <betterfuture032@gmail.com>
2024-07-30 17:42:28 -0600

    refactor: Improve vector manipulation in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,19 @@
-fn get_item(index: usize) {
-    // let index: usize = 3; // This looks like an unsigned integer but it's actually a usize
-    let vec: Vec<i32> = vec![1, 2, 3, 4, 5];
-
-    // Retrieve a value at a specific index
-    let value: &i32 = vec.get(index).unwrap();
-
-    // print the value
-    println!("The value at index {} is: {:?}", index, value);
-}
-
 fn main() {
-    // let vec = vec![];
-    get_item(3);
+    let mut v = vec![1, 2, 3, 4, 5];
+    v.push(6);
+    // println!("{:?}", v); // [1, 2, 3, 4, 5, 6]
 
-    // Retrieve a value at a specific index
-    // let third_value: i32 = vec[2];
-    // println!("The third value in the vector is: {}", third_value);
+    // extend adds each element of the given slice to the vector
+    let more_numbers = vec![7, 8, 9];
+    v.extend(more_numbers);
+    // println!("{:?}", v); // error: value borrowed here after move
 
-    // Retrieve the last value
-    // let last_value: &i32 = vec[vec.len() - 1];
-    // let last_value: &i32 = vec.last().unwrap();
-    // println!("The last value in the vector is: {}", last_value);
+    // append adds the given vector to the vector, requires the vector to be mutable
+    let mut other_numbers = vec![1, 2, 3, 4, 5];
+    v.append(&mut other_numbers);
+    // println!("{:?}", v); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5]
 
-    // Retrieve the first value using pattern matching
-    // match vec.first() {
-    //     Some(first_value) => println!("The first value in the vector is: {}", first_value),
-    //     None => println!("The vector is empty"),
-    // }
-
+    // insert items at a given index
+    v.insert(2, 10);
+    println!("{:?}", v); // [1, 2, 10, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5]
 }
