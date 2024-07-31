a50606503398a1b98f90120c9a409f39090b8c21
Chofor <betterfuture032@gmail.com>
2024-07-30 20:16:44 -0600

    refactor: Improve divide function and error handling in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,16 @@
-#[derive(Debug)]
-enum WineRegions {
-    Bordeaux,
-    Burgundy,
-    Champagne,
-    Loire,
-    Rhone,
-    NapaValley,
-    Tuscany,
-}
-
-struct Wine {
-    name: String,
-    region: WineRegions,
-}
-
-fn supported_regions(wine: WineRegions) {
-    match wine {
-        WineRegions::Bordeaux => println!("Bordeaux is supported!"),
-        WineRegions::Burgundy => println!("Burgundy is supported!"),
-        WineRegions::Champagne => println!("Champagne is supported!"),
-        WineRegions::Loire => println!("Loire is supported!"),
-        WineRegions::Rhone => println!("Rhone is supported!"),
-        WineRegions::NapaValley => println!("Napa Valley is supported!"),
-        WineRegions::Tuscany => println!("Tuscany is supported!"),
+fn divide (x: i32, y: i32) -> Option<i32> {
+    if y == 0 {
+        None
+    } else {
+        Some(x / y)
     }
 }
 
 fn main() {
-    let wine1 = Wine {
-        name: String::from("Chateau Margaux"),
-        region: WineRegions::Bordeaux,
-    };
-
-    let wine2 = Wine {
-        name: String::from("Barolo"),
-        region: WineRegions::Tuscany,
-    };
-
-    // supported_regions(wine1);
-    // println!("Wine 1: {} from {:?}", wine1.name, wine1.region);
-    // println!("Wine 2: {} from {:?}", wine2.name, wine2.region);
-
-    supported_regions(wine1.region);
-    supported_regions(wine2.region);
-    supported_regions(WineRegions::NapaValley);
+    let x = 10;
+    let y = 0;
+    match divide(x, y) {
+        Some(result) => println!("The result is {}", result),
+        None => println!("Cannot divide by zero")
+    }
 }
