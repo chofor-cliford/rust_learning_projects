7635702a9552a316b171848124bd2b5fdb110b47
Chofor <betterfuture032@gmail.com>
2024-08-01 18:37:43 -0600

    refactor: implement exhaustive matches

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,34 @@
-enum Shape {
-    Circle(f64),
-    Rectangle(f64, f64),
-    Square(f64),
+enum WineGrapes {
+    Merlot,
+    CabernetSauvignon,
+    PinotNoir,
+    Chardonnay,
+    SauvignonBlanc,
+    Riesling,
+    Syrah,
+    Zinfandel,
 }
+
+fn taste_wine(grapes: WineGrapes) {
+    match grapes {
+        WineGrapes::Merlot => println!("Merlot: Fruity, medium to full-bodied, dry red wine."),
+        WineGrapes::CabernetSauvignon => println!("Cabernet Sauvignon: Full-bodied, dry red wine."),
+        WineGrapes::PinotNoir => println!("Pinot Noir: Light to medium-bodied, dry red wine."),
+        WineGrapes::Chardonnay => println!("Chardonnay: Full-bodied, dry white wine."),
+        WineGrapes::SauvignonBlanc => println!("Sauvignon Blanc: Dry white wine."),
+        WineGrapes::Riesling => println!("Riesling: Sweet white wine."),
+        WineGrapes::Syrah => println!("Syrah: Full-bodied, dry red wine."),
+        WineGrapes::Zinfandel => println!("Zinfandel: Medium to full-bodied, dry red wine."),
+    }
+}
+
 fn main() {
-   let shapes = vec![
-       Shape::Circle(5.0),
-       Shape::Rectangle(5.0, 2.0),
-       Shape::Square(5.0),
-   ];
-
-   let total_area: f64 = shapes.iter().map(|shape| {
-       match shape {
-           Shape::Circle(radius) => std::f64::consts::PI * radius * radius,
-           Shape::Rectangle(width, height) => width * height,
-           Shape::Square(side) => side * side,
-       }
-   }).sum();
-
-    println!("Total area: {} sq. units", total_area);
+    taste_wine(WineGrapes::Merlot);
+    taste_wine(WineGrapes::Chardonnay);
+    taste_wine(WineGrapes::Riesling);
+    taste_wine(WineGrapes::Syrah);
+    taste_wine(WineGrapes::CabernetSauvignon);
+    taste_wine(WineGrapes::PinotNoir);
+    taste_wine(WineGrapes::Zinfandel);
 }
+ 
