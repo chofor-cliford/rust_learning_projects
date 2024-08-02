842fdb2d318c12d95d6936da36e2428b92cd5211
Chofor <betterfuture032@gmail.com>
2024-08-01 18:27:48 -0600

    Implement enums with vectors

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,22 @@
-enum FileSize {
-    Btyes(u64),
-    KiloBytes(u64),
-    MegaBytes(u64),
-    GigaBytes(u64),
+enum Shape {
+    Circle(f64),
+    Rectangle(f64, f64),
+    Square(f64),
 }
-
-impl FileSize {
-
-fn format_size(&self) -> String {
-     match self {     
-        FileSize::Btyes(bytes) => format!("{} bytes", bytes),
-        FileSize::KiloBytes(kb) => format!("{:.2} KB", *kb as f64/1000.0),
-        FileSize::MegaBytes(mb) => format!("{:.2} MB", *mb as f64/1000.0),
-        FileSize::GigaBytes(gb) => format!("{:.2} GB", *gb as f64/1000.0),
-        }
-    }
-}
-
 fn main() {
-    let size = 4100000000;
-    let filesize = match size {
-        0..=999 => FileSize::Btyes(size),
-        1000..=999_999 => FileSize::KiloBytes(size / 1000),
-        1_000_000..=999_999_999 => FileSize::MegaBytes(size / 1_000_000),
-        _ => FileSize::GigaBytes(size / 1_000_000_000),
-    };
+   let shapes = vec![
+       Shape::Circle(5.0),
+       Shape::Rectangle(5.0, 2.0),
+       Shape::Square(5.0),
+   ];
 
-    println!("File size: {}", filesize.format_size());
+   let total_area: f64 = shapes.iter().map(|shape| {
+       match shape {
+           Shape::Circle(radius) => std::f64::consts::PI * radius * radius,
+           Shape::Rectangle(width, height) => width * height,
+           Shape::Square(side) => side * side,
+       }
+   }).sum();
+
+    println!("Total area: {} sq. units", total_area);
 }
